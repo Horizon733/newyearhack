15026eabcc87b6d53277971134b258d1c4b7f111
Ryan Cohen <ryjco20@gmail.com>
2021-01-01 12:46:21 -0500

    Splash and profile screens

---
+++ app/src/main/java/com/example/newyearhack/Splash.kt
@@ -0,0 +1,13 @@
+package com.example.newyearhack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
