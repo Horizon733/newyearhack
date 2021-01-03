5cbf9c72cd1f74f331476ec47dc4328c0c5a895d
Horizon733 <dishantgandhi733@gmail.com>
2021-01-03 10:16:54 +0530

    fixed some bugs and statusbar color

--- app/src/main/java/com/example/newyearhack/Splash.kt
+++ app/src/main/java/com/example/newyearhack/Splash.kt
@@ -1,17 +1,22 @@
 package com.example.newyearhack
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
+import android.view.Window
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.activity_splash.viewPager
 import me.relex.circleindicator.CircleIndicator3
 
 class Splash : Fragment(){
-
+    private val actionbar = activity?.actionBar
+    private lateinit var window:Window
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window = activity?.window!!
+    }
     companion object{
         fun newInstance(): Splash{
             return Splash()
@@ -38,13 +43,14 @@ class Splash : Fragment(){
         )
         val view = inflater.inflate(R.layout.activity_splash, container, false)
 
-        val adapter = ViewPager(labels, images)
+        val adapter = ViewPager(activity as MainActivity,labels, images,window,color)
         val viewPager: ViewPager2 = view.findViewById(R.id.viewPager)
         viewPager.adapter = adapter
 
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 view.setBackgroundResource(color[position])
+                window.statusBarColor = ContextCompat.getColor(activity as MainActivity,color[position])
             }
         })
 
--- app/src/main/java/com/example/newyearhack/ViewPager.kt
+++ app/src/main/java/com/example/newyearhack/ViewPager.kt
@@ -3,12 +3,14 @@ package com.example.newyearhack
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.item_page.view.*
 
 
-class ViewPager(private var labels : List<String>, private var images : List<Int>) : RecyclerView.Adapter<ViewPager.ViewPagerHolder>(){
+class ViewPager(var activity: MainActivity, var labels : List<String>, private var images : List<Int>,var window: Window, var color:List<Int>) : RecyclerView.Adapter<ViewPager.ViewPagerHolder>(){
 
     inner class ViewPagerHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
@@ -26,6 +28,7 @@ class ViewPager(private var labels : List<String>, private var images : List<Int
     override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
         val curImage = images[position]
         val curLabel = labels[position]
+        val curColor = color[position]
         holder.itemView.viewImage.setImageResource(curImage);
         holder.itemView.viewLabel.setText(curLabel)
     }
